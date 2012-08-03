565ed80e8ce4af37cec32360104ff08d3cecb215
Mario Rosales <mario.rosales@mobivery.com>
2012-08-03 14:05:11 +0200

    Arc configuration

---
+++ iHappiness-iOS/noArcLibreries/noArcLibreries.h
@@ -0,0 +1,13 @@
+//
+//  noArcLibreries.h
+//  noArcLibreries
+//
+//  Created by Mario Rosales Maillo on 03/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface noArcLibreries : NSObject
+
+@end
---
+++ iHappiness-iOS/noArcLibreries/noArcLibreries.m
@@ -0,0 +1,23 @@
+//
+//  noArcLibreries.m
+//  noArcLibreries
+//
+//  Created by Mario Rosales Maillo on 03/08/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "noArcLibreries.h"
+
+@implementation noArcLibreries
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
