9e61a7c5c808ec7a87ce74dfa3fa418340a7b781
Pedro Duran <pedro.duran@mobivery.com>
2012-08-03 17:40:44 +0200

    Revert "Revert "Removed call from general ViewController""
    This reverts commit 7331d517dc9a10b9509332fecc203eaa418aaab8.

--- iHappiness-iOS/iHappiness-iOS/ViewController.m
+++ iHappiness-iOS/iHappiness-iOS/ViewController.m
@@ -18,8 +18,6 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-    
-    [[LoginDAO sharedInstance] requestLoginWithUser:@"pedroduran" andPassword:@"rocko#23"];
 }
 
 - (void)viewDidUnload
